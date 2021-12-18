e191402504c6d10ebd7dbd232613f0c44e36d30f
paige <paige@paiges-Mac-mini.local>
2021-12-19 07:39:27 +0900

    init

---
+++ 6Circles-scale-move-rotate/AnimCollectionRepo/AnimCollectionRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimCollectionRepoApp.swift
+//  AnimCollectionRepo
+//
+//  Created by paige on 2021/12/19.
+//
+
+import SwiftUI
+
+@main
+struct AnimCollectionRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
