ae37d082ca189aaff3633ce5a2f243f5c31cbd16
paige <paige@paiges-Mac-mini.local>
2021-12-19 08:18:38 +0900

    added ref

---
+++ RecordPlayer/ref/Record Player/Record Player/Record_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Record_PlayerApp.swift
+//  Record Player
+//
+//  Created by Stephen DeStefano on 10/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Record_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
