11b43512c388590ad04bd0caa199872da1778d1f
paige <paige@paiges-Mac-mini.local>
2021-12-20 08:58:42 +0900

    stars

---
+++ StarryStarryNight/StarryStarryNight/StarryStarryNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarryStarryNightApp.swift
+//  StarryStarryNight
+//
+//  Created by paige on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct StarryStarryNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StarryStarryNight/ref/Twinkling Stars/Twinkling Stars/Twinkling_StarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twinkling_StarsApp.swift
+//  Twinkling Stars
+//
+//  Created by Stephen DeStefano on 11/5/20.
+//
+
+import SwiftUI
+
+@main
+struct Twinkling_StarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
