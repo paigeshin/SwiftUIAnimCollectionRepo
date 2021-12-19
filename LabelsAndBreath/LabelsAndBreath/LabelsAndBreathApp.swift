7eab7453cd8faafc5107ca00fd4d0ab5d35d128a
paige <paige@paiges-Mac-mini.local>
2021-12-20 03:56:18 +0900

    breath

---
+++ LabelsAndBreath/LabelsAndBreath/LabelsAndBreathApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelsAndBreathApp.swift
+//  LabelsAndBreath
+//
+//  Created by paige on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct LabelsAndBreathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LabelsAndBreath/ref/Breathing Flower/Breathing Flower/Breathing_FlowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breathing_FlowerApp.swift
+//  Breathing Flower
+//
+//  Created by Stephen DeStefano on 10/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Breathing_FlowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
