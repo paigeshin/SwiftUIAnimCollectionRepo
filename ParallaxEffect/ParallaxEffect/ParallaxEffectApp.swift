7e1f566a230c232a9b35154738e30a606f9e049a
paige <paige@paiges-Mac-mini.local>
2021-12-20 06:12:01 +0900

    3D rotation effect

---
+++ ParallaxEffect/ParallaxEffect/ParallaxEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxEffectApp.swift
+//  ParallaxEffect
+//
+//  Created by paige on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ParallaxEffect/ref/Parallax Effect/Parallax Effect/Parallax_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parallax_EffectApp.swift
+//  Parallax Effect
+//
+//  Created by Stephen DeStefano on 11/4/20.
+//
+
+import SwiftUI
+
+@main
+struct Parallax_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
