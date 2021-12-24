7c05f38712f12b1c0e0df5709e8f6116fdb12485
paige <paige@paiges-Mac-mini.local>
2021-12-25 03:25:28 +0900

    success

---
+++ LightSwitch/LightSwitch/LightSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightSwitchApp.swift
+//  LightSwitch
+//
+//  Created by paige on 2021/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LightSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
