9eb74380e2f6928ddeed5a61768ef5001d91270f
paige <paige@paiges-Mac-mini.local>
2021-12-25 03:13:47 +0900

    linestroke

---
+++ heart_line_stroke_anim/heart_line_stroke_anim/heart_line_stroke_animApp.swift
@@ -0,0 +1,17 @@
+//
+//  heart_line_stroke_animApp.swift
+//  heart_line_stroke_anim
+//
+//  Created by paige on 2021/12/25.
+//
+
+import SwiftUI
+
+@main
+struct heart_line_stroke_animApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
